967ba00111b84c569baf48b7755fddd301f42810
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 15:04:53 -0400

    Carthage-ready Xcode project.

---
+++ JTSImageViewController/JTSImageViewController.h
@@ -0,0 +1,19 @@
+//
+//  JTSImageViewController.h
+//  JTSImageViewController
+//
+//  Created by John Brayton on 6/26/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTSImageViewController.
+FOUNDATION_EXPORT double JTSImageViewControllerVersionNumber;
+
+//! Project version string for JTSImageViewController.
+FOUNDATION_EXPORT const unsigned char JTSImageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSImageViewController/PublicHeader.h>
+
+
