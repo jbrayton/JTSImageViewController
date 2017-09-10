f1f6b3d4009cc74dba168ec400b2db4be1644b83
John Brayton <jbrayton@goldenhillsoftware.com>
2017-09-10 15:20:29 -0400

    more tweaks

--- Source/JTSImageViewControllerObj.m
+++ Source/JTSImageViewControllerObj.m
@@ -496,7 +496,7 @@ typedef struct {
 }
 
 - (void)viewDidLoadForAltTextMode {
-    
+    self.view.alpha = 0.0;
     self.view.backgroundColor = [UIColor blackColor];
     self.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     
@@ -700,16 +700,9 @@ typedef struct {
                      } else {
                          scaling = JTSImageViewController_MinimumBackgroundScaling;
                      }
-//                     weakSelf.snapshotView.transform = CGAffineTransformConcat(weakSelf.snapshotView.transform, CGAffineTransformMakeScale(scaling, scaling));
                      
-                     if (weakSelf.backgroundOptions & JTSImageViewControllerBackgroundOption_Blurred) {
-//                         weakSelf.blurredSnapshotView.alpha = 1;
-                     }
-                     
-                     if (weakSelf.backgroundOptions & JTSImageViewControllerBackgroundOption_Scaled) {
-                         [weakSelf addMotionEffectsToSnapshotView];
-                     }
                      weakSelf.blackBackdrop.alpha = self.alphaForBackgroundDimmingOverlay;
+                     weakSelf.view.alpha = 1.0;
                      
                      if (mustRotateDuringTransition) {
                          weakSelf.imageView.transform = CGAffineTransformIdentity;
